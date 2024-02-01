3b402ece9069a208401400576903fc88fe6f944f
Alkrax87 <thedarkdawn87gtamc@gmail.com>
2024-01-31 19:57:23 -0500

    Implementacion del Dashboard

---
+++ dashboard.js
@@ -0,0 +1,7 @@
+console.log("=========");
+console.log("Dashboard");
+console.log("=========");
+console.log("   User  ");
+console.log("=========");
+console.log("   DATA  ");
+console.log("=========");
